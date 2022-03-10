df30283a2e472585b5015193ee28394ce6f177bb
Kim Godard <kim.godard@snapdocs.com>
2022-03-09 17:32:18 -0700

    Update issue class to init by rest obj

--- jira_issue.rb
+++ jira_issue.rb
@@ -4,12 +4,11 @@ require 'jira-ruby'
 
 class JiraIssue
 
-  attr_reader :key, :client, :created, :summary, :description, :status, :fields, :histories
+  attr_reader :key, :client, :created, :summary, :description, :status, :fields, :histories,
+    :assignee, :type, :raw, :subtasks, :parent
 
-  def initialize(key:, options: default_options)
-    @key = key
-    @client = JIRA::Client.new(options)
-    fetch_and_load
+  def initialize(jira_issue)
+    load_issue(jira_issue)
   end
 
   def cycle_time_in_days
@@ -45,37 +44,42 @@ class JiraIssue
     end
   end
 
+  def parent_type
+    parent["fields"]["issuetype"]["name"]
+  end
+
   def status_histories
     histories.select {|hist| hist["items"].map {|i| i["field"]}.include?("status")}
   end
 
-  def fetch_and_load
-    load_issue(jira_fetch)
+  def parent_summary
+    parent["fields"]["summary"]
   end
 
   def load_issue(jira_issue)
     if jira_issue.nil?
       raise "JIRA issue #{key} was not found!"
     else
+      @raw         = jira_issue
+      @key         = jira_issue.key
+      @type        = jira_issue.issuetype.name
+      @assignee    = jira_issue.assignee.displayName rescue nil
       @created     = jira_issue.created
       @summary     = jira_issue.summary
       @description = jira_issue.description
       @status      = jira_issue.status.name
       @fields      = jira_issue.fields
       @histories   = jira_issue.changelog["histories"]
+      @subtasks    = jira_issue.subtasks
+      @parent      = jira_issue.parent
     end
   end
 
-  def jira_fetch
-    response = client.Issue.jql("key = #{key}", {expand: 'changelog'})
-    response.first rescue nil
-  end
-
-  def default_options
-    { username: 'kim.godard@snapdocs.com',
-      password: ENV["JIRA_TOKEN"],
-      site: 'http://snapdocs-eng.atlassian.net:443/',
-      context_path: '',
-      auth_type: :basic }
-  end
+  # def default_options
+  #   { username: 'kim.godard@snapdocs.com',
+  #     password: ENV["JIRA_TOKEN"],
+  #     site: 'http://snapdocs-eng.atlassian.net:443/',
+  #     context_path: '',
+  #     auth_type: :basic }
+  # end
 end
